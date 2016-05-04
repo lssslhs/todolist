1aad0697574cd9f02b72948e86ea0e50bf751a36
Michael <lssslhs@gmail.com>
2016-05-03 19:43:42 -0700

    fix time format

--- src/client/app/todolist/todolist.filter.js
+++ src/client/app/todolist/todolist.filter.js
@@ -6,11 +6,21 @@
 
 	function timefilter() {
 		return function(time) {
+			if (typeof(time) === 'string') {
+				var date = time.split('T')[0].split('-').join('/');
+				var timeTokens = time.split('T')[1].split(':');
 
-			var date = time.split('T')[0].replace('-','/');
-			var timeTokens = time.split('T')[1].split(':');
+				return date + ' ' + timeTokens[0] + ':' + timeTokens[1];
+			}
+			else {
 
-			return date + ' ' + timeTokens[0] + ':' + timeTokens[1];
+				var year = time.getFullYear();
+				var month = time.getMonth()+1 < 10 ? '0' + (time.getMonth()+1) : (time.getMonth()+1);
+				var date = time.getDate() < 10 ? '0' + time.getDate() : time.getDate();
+
+				return year + '/' + month + '/' + date
+						+ ' ' + time.getHours() + ':' + time.getMinutes();
+			}
 		}
 	}
 }();
